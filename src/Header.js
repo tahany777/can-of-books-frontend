60ced58a83bd3ccf316cd68ff651e9c305e0c128
tahany777 <tahanymohammed777@hotmail.com>
2021-08-15 18:50:07 +0300

    auth

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from "react-router-dom";
 import './Header.css';
-
+import LogoutButton from './components/LogoutButton';
+import LoginButton from './components/LoginButton';
+import { withAuth0 } from '@auth0/auth0-react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 class Header extends React.Component {
   render() {
     return(
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        <Container>
+            <Navbar.Brand href="#home">My Favorite Books</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+            </Nav>
+            {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
+            {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        </Container>
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import './Login.css';
+import LoginButton from './components/LoginButton';
+
+
 
 class Login extends React.Component {
   render() {
@@ -12,6 +15,7 @@ class Login extends React.Component {
           <Card.Text>
             Click Below to Log In
           </Card.Text>
+          <LoginButton />
           {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
         </Card.Body>
       </Card>
