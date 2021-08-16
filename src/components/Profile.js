53b148822ae5ef8167b8f126dd4e257604661179
tahany777 <tahanymohammed777@hotmail.com>
2021-08-16 22:39:14 +0300

    best books

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,38 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React, { Component } from "react";
+import { useAuth0, withAuth0 } from "@auth0/auth0-react";
 import { Card } from "react-bootstrap";
-
-const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <div>Loading ...</div>;
+import axios from 'axios';
+class Profile extends Component {
+  componentDidMount = () => {
+    if(this.props.auth0.isAuthenticated) {
+      this.props.auth0.getIdTokenClaims().then( result => {
+        const jwt = result.__raw;
+        const config = {
+          headers: {"Authorization" : `Bearer ${jwt}`},
+          method: 'get',
+          baseURL: process.env.REACT_APP_SERVER_URL,
+          url: '/test'
+        }
+        axios(config)
+        .then(axiosResults => console.log(axiosResults.data))
+        .catch(err => console.error(err))
+      })
+      .catch(err => console.error(err));
+    }
   }
-  return (
-    isAuthenticated && (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={user.picture} alt={user.name}/>
+  render(){
+    return (
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.auth0.user.picture} alt={this.props.auth0.user.name}/>
         <Card.Body>
-        <Card.Text>{user.name}</Card.Text>
-        <Card.Text>{user.email}</Card.Text>
+        <Card.Text>{this.props.auth0.user.name}</Card.Text>
+        <Card.Text>{this.props.auth0.user.email}</Card.Text>
         </Card.Body>
-        
         </Card>
-    )
-  );
-};
+        )
+  }
+}
 
-export default Profile;
+
+export default withAuth0(Profile);
 //
